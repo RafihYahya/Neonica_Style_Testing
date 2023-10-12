6bbab971077a7bf85d5d1102ba868ed4c6de575d
RafihYahya <rafihyahya@gmail.com>
2023-10-12 15:49:13 +0100

    here_we_go

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
+const play1 = document.getElementById("anima");
+const play2 = document.getElementById("anima2");
+const ball = document.getElementById("anima3");
 
-play1 = document.getElementById('anima');
-vartop = play1.style.top = '10px';
-
-const animation_loop =  () => {
-     for (let i = 0; i < 10; i++) {
-        setTimeout(() => {
-            play1.style.top = i + '%';
-            console.log(play1.style.top);
-
-        } ,2000);
-       
-        
-    }
+let play1_pos = {
+  top: "0",
+  left: "0",
+  right: "0",
+  left: "0",
 };
 
+let play2_pos = {
+  top: "0",
+  left: "0",
+  right: "0",
+  left: "0",
+};
+let ball_pos = {
+  top: "0",
+  left: "0",
+  right: "0",
+  left: "0",
+};
 
- animation_loop();
+const gameloop = () => {
+  let f = 0;
+  while (f != 1) {
+    console.log("1");
+  };
+};
